6f7ea94790a0ffd3c38e0583046d17ba2452c9a7
Andy Clegg <andy2.0@gmail.com>
2011-02-17 18:19:08 +0000

    Remove result_set_prepare_iteration, update notepad (remove fixed items)

--- src/result_set.h
+++ src/result_set.h
@@ -18,7 +18,7 @@ result_set_t *result_set_init();
 void result_set_insert(result_set_t *set, float x, float y, int record_index);
 void result_set_free(result_set_t *set);
 void print_result_set(result_set_t *set);
-void result_set_prepare_iteration(result_set_t *set);
+void result_set_reset_iteration(result_set_t *set);
 struct result_set_item *result_set_iterate(result_set_t *set);
 unsigned int result_set_len(result_set_t *set);
 #endif
